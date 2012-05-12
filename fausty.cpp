fa55bcc5c4262a6ceda302d2ed0fea65e0f0b5e1
Harry van Haaren <harryhaaren@gmail.com>
2012-05-12 13:35:04 +0100

    -Faust compile working, added test.dsp file

--- fausty.cpp
+++ fausty.cpp
@@ -3,13 +3,32 @@
 // compile:  g++ -o fausty fausty.cpp  `pkg-config --cflags --libs gtkmm-2.4 gtksourceviewmm-2.0`
 
 #include <iostream>
+#include <sstream>
 
 #include <gtkmm.h>
 #include <gtksourceviewmm.h>
 
-
-
 using namespace std;
+
+// if compiling test.dsp, name should just be "test"
+void compile(std::string name)
+{
+  std::stringstream command;
+  
+  command << "faust -a jack-gtk.cpp -o ";
+  command << name;
+  command << ".cpp ";
+  command << name;
+  command << ".dsp";
+  
+  int returnStatus = -1234;
+  std::string cout, cerr;
+  
+  Glib::spawn_command_line_sync( command.str() , &cout, &cerr, &returnStatus );
+  
+  std::cout << "Faust returned " << returnStatus << std::endl << cout << std::endl<< std::endl << cerr << std::endl;;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -18,6 +37,8 @@ int main(int argc, char** argv)
   gtksourceview::init () ;
   
   Gtk::Window window;
+  Gtk::Button button("Compile");
+  button.signal_clicked().connect( sigc::bind (sigc::ptr_fun( &compile ), "test") );
   
   gtksourceview::SourceView sourceview ;
   
@@ -36,6 +57,7 @@ int main(int argc, char** argv)
   
   Gtk::HBox box;
   box.add(sourceview);
+  box.add(button);
   box.add(*image);
   
   window.add(box);
